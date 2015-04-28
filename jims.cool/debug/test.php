ffb1189e186f9c81beb4916a8b8df4825c1b3d6f
weasy <jim@weasy.net>
2015-04-28 17:08:58 +0000

    commited jims.cool

---
+++ jims.cool/debug/test.php
@@ -0,0 +1,13 @@
+<?php 
+echo "Hello World"."\n";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo date( 'm/d/y h:ia' ); 
+echo "<br>";
+echo "<br>";
+$var = '122.34343The';
+$float_value_of_var = floatval($var);
+echo $float_value_of_var; // 122.34343
+?>
+
